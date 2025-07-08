bb937d0dda68df17f486f53496476a64c10f271a
Brian Park <jin9122@gmail.com>
2025-07-08 14:40:19 -0400

    Add additional Google Drive directory variables

--- recursive_file_copy.py
+++ recursive_file_copy.py
@@ -3,17 +3,25 @@ import os
 
 # Reference Dir Paths
 
-# Google Drive
+# PSP
 psp_cn_dir = '/Users/brianpark/Library/CloudStorage/GoogleDrive-bpark@alkymi.io/Shared drives/Clients (contains client data)/Public Sector Pension Investment Board (PSP)/5 - Data/0 - Capital Notice'
 psp_cas_dir = '/Users/brianpark/Library/CloudStorage/GoogleDrive-bpark@alkymi.io/Shared drives/Clients (contains client data)/Public Sector Pension Investment Board (PSP)/5 - Data/1 - Capital Account Statement (CAS)'
 psp_soi_dir = '/Users/brianpark/Library/CloudStorage/GoogleDrive-bpark@alkymi.io/Shared drives/Clients (contains client data)/Public Sector Pension Investment Board (PSP)/5 - Data/2 - Schedule of Investments (SOI)'
+
+# SSBI 
 ssbi_cn_dir = '/Users/brianpark/Library/CloudStorage/GoogleDrive-bpark@alkymi.io/Shared drives/Clients (contains client data)/SimCorp/Use Cases/Capital Notices/Capital Call Data/StateStreet'
 
-# source_dir = '/Users/brianpark/Library/CloudStorage/GoogleDrive-bpark@alkymi.io/Shared drives/Clients (contains client data)/Public Sector Pension Investment Board (PSP)/5 - Data/0 - Capital Notice'
-source_dir = psp_soi_dir
+# TRS
+trs_cn_enri_dir = '/Users/brianpark/Library/CloudStorage/GoogleDrive-bpark@alkymi.io/Shared drives/Clients (contains client data)/Teacher Retirement System of Texas (TRS)/3 - Data/ENRI - Energy, Natural Resources, and Infrastructure'
+trs_cn_re_dir = '/Users/brianpark/Library/CloudStorage/GoogleDrive-bpark@alkymi.io/Shared drives/Clients (contains client data)/Teacher Retirement System of Texas (TRS)/3 - Data/RE – Real Estate'
+trs_cn_pe_dir = '/Users/brianpark/Library/CloudStorage/GoogleDrive-bpark@alkymi.io/Shared drives/Clients (contains client data)/Teacher Retirement System of Texas (TRS)/3 - Data/PE - Private Equity'
+trs_cn_epu_dir = '/Users/brianpark/Library/CloudStorage/GoogleDrive-bpark@alkymi.io/Shared drives/Clients (contains client data)/Teacher Retirement System of Texas (TRS)/3 - Data/EPU - External Public Markets'
 
-dest_dir = '/Users/brianpark/Documents/Work/Dev/py - Copy Local Files/destination/PSP/Schedule of Investments/By Asset Class/PSP_SOI_LLM_NaturalResources'
-file_list_path = '/Users/brianpark/Documents/Work/Dev/py - Copy Local Files/file_lists/PSP/Schedule of Investments/By Asset Class/PSP_SOI_LLM_NaturalResources.txt'
+# source_dir = '/Users/brianpark/Library/CloudStorage/GoogleDrive-bpark@alkymi.io/Shared drives/Clients (contains client data)/Public Sector Pension Investment Board (PSP)/5 - Data/0 - Capital Notice'
+source_dir = trs_cn_epu_dir
+
+dest_dir = '/Users/brianpark/Documents/Work/Dev/py - Copy Local Files/destination/TRS/Capital Notice/EPU_GapAnalysis'
+file_list_path = '/Users/brianpark/Documents/Work/Dev/py - Copy Local Files/file_lists/TRS/Capital Notice/Gap Analysis/EPU_GapAnalysis_07082025.txt'
 
 # Read the list of files to copy
 with open(file_list_path, "r") as file:
